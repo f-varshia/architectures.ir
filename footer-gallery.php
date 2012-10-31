11deee14b6d9c34159e80793c3b7fbd81c14023c
f-varshia <vasha.fateme@gmail.com>
2012-10-31 15:23:55 -0700

    change page library (just gallery jquery)

---
+++ footer-gallery.php
@@ -0,0 +1,11 @@
+<div class="container-footer">
+  <div class="footer">
+    <p>Design 2011</p>
+  </div>
+</div>
+<?php get_template_part("contact"); ?>
+<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/jquery.js"></script>
+<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/jquery-ui.js"></script>
+<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/lightbox.js"></script>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -4,9 +4,5 @@
   </div>
 </div>
 <?php get_template_part("contact"); ?>
-<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/script.js"></script>
-<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/jquery.js"></script>
-	<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/jquery-ui.js"></script>
-	<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/lightbox.js"></script>
 </body>
 </html>
