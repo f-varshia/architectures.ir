8a2aea065ea651ab4444ff77f6e45b0110c43c45
f-varshia <vasha.fateme@gmail.com>
2012-10-31 01:33:16 -0700

    change gallery image by custom post image

--- gallery.php
+++ gallery.php
@@ -11,21 +11,20 @@
 <div class="wall-2"></div>
     <div class="page">
         <div id="scroll">
-        <?php
-            while(have_posts()){
-                the_post();
-                $meta = get_post_custom();
-        ?>
-            <article>
-                <figure><?php 
-                    the_content();
+        
+        <figure><?php
+    $a='#';
+                  the_post();
+                  $meta = get_post_custom();
+                  if(count($meta['img'])>0){
+                  foreach ($meta['img'] as $img_id) {
+                    $img = wp_get_attachment_image($img_id,'large'); 
+                    echo "<div class='frame'><div class='frame-pic'><a href='$a' rel='lightbox[group1]'>$img</a></div></div>";
+                  }
+                  }
                 ?></figure>
-            </article>
-    <?php
-        };
-    ?>
         </div>
-</div>
+    </div>
 <div id="fade" class="black_overlay">
 </div> 
 <?php 
