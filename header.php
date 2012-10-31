1864b395f3e757143fb9600573236111c5eee7ea
f-varshia <vasha.fateme@gmail.com>
2012-10-31 16:59:11 -0700

    remove common-files

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8" />
 <title><?php bloginfo('name'); wp_title(); ?></title>
 <link type="text/css" rel="stylesheet" href="<?php bloginfo("template_url"); ?>/style.css" />
-<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/common-files/zepto.js"></script>
+<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/zepto.js"></script>
 <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/script.js"></script>
 <link type="text/css" rel="stylesheet" href="<?php bloginfo("template_url"); ?>/lightbox.css" />
 <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/light-old.js"></script>
